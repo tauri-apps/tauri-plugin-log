0fc24102ef8db94e6c6ef1ef6ead62bf1065310a
Horacio Herrera <hi@horacioh.com>
2022-01-18 16:09:53 +0100

    fix logging on cra

--- examples/cra/src-tauri/src/main.rs
+++ examples/cra/src-tauri/src/main.rs
@@ -3,8 +3,18 @@
   windows_subsystem = "windows"
 )]
 
+use tauri_plugin_log::{LogTarget, LoggerBuilder};
+
 fn main() {
   tauri::Builder::default()
+  .plugin(
+    LoggerBuilder::new([
+      // LogTarget::AppDir("./logs".into()),
+      LogTarget::Stdout,
+      LogTarget::Webview,
+    ])
+    .build(),
+  )
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
