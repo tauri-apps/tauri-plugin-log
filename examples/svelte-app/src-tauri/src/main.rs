6996322615f39d22671d5df41385b54bfd687d5f
Jonas Kruckenberg <iterpre@protonmail.com>
2022-01-18 19:41:51 +0100

    docs: make svelte example use colors

--- examples/svelte-app/src-tauri/src/main.rs
+++ examples/svelte-app/src-tauri/src/main.rs
@@ -3,13 +3,15 @@
   windows_subsystem = "windows"
 )]
 
-use tauri_plugin_log::{LogTarget, LoggerBuilder};
+use tauri_plugin_log::{fern::colors::ColoredLevelConfig, LogTarget, LoggerBuilder};
 
 fn main() {
   let targets = [LogTarget::LogDir, LogTarget::Stdout, LogTarget::Webview];
 
+  let colors = ColoredLevelConfig::default();
+
   tauri::Builder::default()
-    .plugin(LoggerBuilder::new(targets).build())
+    .plugin(LoggerBuilder::with_colors(colors).targets(targets).build())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
