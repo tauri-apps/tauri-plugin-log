775f4b843d26a4823f63ac3ffc836d0920a415a9
Horacio Herrera <hi@horacioh.com>
2022-01-18 21:51:35 +0100

    feat(example): add CRA example
    fix(package): add exports to package.json
    
    wip. tauri build failing
    
    fix logging on cra

---
+++ examples/cra/src-tauri/src/main.rs
@@ -0,0 +1,20 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use tauri_plugin_log::{LogTarget, LoggerBuilder};
+
+fn main() {
+  tauri::Builder::default()
+  .plugin(
+    LoggerBuilder::new([
+      // LogTarget::AppDir("./logs".into()),
+      LogTarget::Stdout,
+      LogTarget::Webview,
+    ])
+    .build(),
+  )
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
