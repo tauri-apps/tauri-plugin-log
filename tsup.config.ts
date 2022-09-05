09b885e7749573ef8f44073eee402704d56ea303
Jonas Kruckenberg <iterpre@protonmail.com>
2022-09-05 17:12:10 +0200

    Revert "chore: switch to tsup"
    This reverts commit c8c0d5b44b8bd2643292932e1fa17aacc0a9f219.

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-    entry: ["webview-src/index.ts"],
-    format: ["esm"],
-    outDir: "webview-dist",
-    clean: true,
-    dts: true
-});
